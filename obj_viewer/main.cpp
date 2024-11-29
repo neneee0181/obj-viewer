6ddd1f73e02aeb0c411e49b5142bfc32bef79ef9
neneee0181 <2022180024@tukorea.ac.kr>
2024-11-29 23:50:42 +0900

    초기 커밋 (리드미 제외)

---
+++ obj_viewer/main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <vector> 
+#include <atomic>
+
+#include "shaderMaker.h"
+#include "DefaultModel.h"
+#include "KeyBoard.h"
+#include "Camera.h"
+#include "Light.h"
+#include "Debug.h"
+
+using namespace std;
+
+void InitBuffer();
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+//모델
+vector<Model*> models;
+
+int main(int argc, char** argv) {
+
+    // 모델 정보를 사용자로부터 입력받기
+    string modelPath;
+    string modelName;
+    string modelType;
+    glm::vec3 modelScale;
+    string debut_status;
+
+    cout << "Enter model file path (ex> obj/Skull.obj): ";
+    cin >> modelPath;
+    cout << "Enter model name (ex> Skull): ";
+    cin >> modelName;
+    cout << "Enter model type (ex> bone): ";
+    cin >> modelType;
+    cout << "Enter model scale (ex>, 1.0 1.0 1.0): ";
+    cin >> modelScale.x >> modelScale.y >> modelScale.z;
+    cout << "Enter Debug mode (ex> y or n):";
+    cin >> debut_status;
+
+    width = 800;
+    height = 600;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowPosition(100, 200);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("obj viewer");
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        cerr << "Unable to initialize GLEW" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+        cout << "GLEW Initialized\n";
+
+    make_shaderProgram();
+
+    DefaultModel* default_model = new DefaultModel(
+        modelPath, modelName, modelType,
+        glm::scale(glm::mat4(1.0f), modelScale)
+    );
+
+    models.push_back(default_model);
+
+    // 디버깅 출력
+    if (debut_status == "y")
+        debug_model(default_model);
+
+    InitBuffer();   
+
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(Reshape);
+    glutKeyboardFunc(keyDown);
+    glutKeyboardUpFunc(keyUp);
+    glutSpecialFunc(keySpecial);
+    glutMainLoop(); 
+
+    return 0;
+}
+
+GLvoid drawScene() {
+
+    glClearColor(1.0, 1.0, 1.0, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+    glUseProgram(shaderProgramID);
+
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR) {
+        std::cout << "Error in glUseProgram: " << error << std::endl;
+    }
+
+    view = glm::mat4(1.0f);
+    view = glm::lookAt(
+        cameraPos,
+        cameraDirection,
+        cameraUp
+    );
+    unsigned int viewLocation = glGetUniformLocation(shaderProgramID, "viewTransform");
+    glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
+
+    projection = glm::mat4(1.0f);
+    projection = glm::perspective(glm::radians(45.0f), static_cast<float>(width) / static_cast<float>(height), 0.1f, 300.0f);
+    unsigned int projectionLocation = glGetUniformLocation(shaderProgramID, "projectionTransform");
+    glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, &projection[0][0]);
+
+    GLint lightPosLoc = glGetUniformLocation(shaderProgramID, "lightPos");
+    GLint lightColorLoc = glGetUniformLocation(shaderProgramID, "lightColor");
+    glUniform3fv(lightPosLoc, 1, glm::value_ptr(lightPos));
+    glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
+   
+    glEnable(GL_DEPTH_TEST);
+    for (const auto& model : models) { // 실제 모델 draw
+        model->draw(shaderProgramID, isKeyPressed_s);
+    }
+    glDisable(GL_DEPTH_TEST);
+
+    glutSwapBuffers();
+
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+        cout << "OpenGL error: " << err << endl;
+    }
+}
+
+// 버퍼 초기화 함수
+void InitBuffer() {
+    //-----------------------------------------------------------------------------------------------------------
+    for (const auto& model : models) { // 모든 모델 initBuffer
+        model->initBuffer();
+    }
+}
